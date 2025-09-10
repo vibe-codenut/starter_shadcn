5a97bee4d597fb062aa5794c2edd91622928f687
Peng Deng <peng.deng@metatrust.io>
2025-09-10 18:21:09 +0800

    chore: a little adjustment for the app starter template

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@/components/ui/button';
-
 const Index = () => {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen w-full flex-col bg-gradient-to-b from-muted to-muted/50 items-center justify-center p-24">
       <div className="z-10 text-center">
-        <Button>Welcome to Starter Website</Button>
+        <h1 className="text-4xl font-bold">Welcome to Your App Starter</h1>
+        <p className="text-2xl mt-8 text-muted-foreground">
+          Let&apos;s build something amazing together!
+        </p>
       </div>
     </main>
   );
